2ed4becf6d1cd7550df12382c6f50a07c0cf4ce9
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-12 18:32:43 -0300

    feat: strings

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Testando texto de aspas duplas <br>";
+echo 'Testando texto de aspas simples <br>';
+echo "Ele disse 'olá!' <br>";
+
+$idade = 15;
+
+echo "Ele tem $idade anos <br>";
+echo 'Ele tem $idade anos';
---
+++ 3_tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Pedro";
+$profissao = "Desenvolvedor";
+
+echo "O " . $nome . " é um " . $profissao . " muito bom! <br>";
+
+echo '"aspas" simples <br>';
+echo "'aspas' duplas";
