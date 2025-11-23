429079619ee93313a84c029e55f859ffd5690e1a
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-23 14:07:28 -0300

    feat: operadores aritmeticos

---
+++ 5_expressoes_e_operacoes/5_operadores_aritmeticos/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$soma = 5 + 5;
+echo $soma . "<br>";
+
+$subtracao = 10 - 2;
+echo $subtracao . "<br>";
+
+$mult = 2 * 199;
+echo $mult . "<br>";
+
+$divisao = 123 / 48;
+echo $divisao . "<br>";
+
+$op = 5 + (2 / 7) * 8;
+echo $op . "<br>";
