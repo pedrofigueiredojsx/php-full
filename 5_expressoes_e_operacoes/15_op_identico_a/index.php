6a7cd54f8f2a41042140b9635614db5457691bf1
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-30 00:14:30 -0300

    feat: operador de identico

---
+++ 5_expressoes_e_operacoes/15_op_identico_a/index.php
@@ -0,0 +1,31 @@
+<?php
+
+if (5 == '5') {
+  echo '5 é 5 <br>';
+}
+
+// op de identico
+if (5 === '5') {
+  echo '5 é 5 2<br>';
+}
+
+if (5 === 5) {
+  echo '5 é 5 3<br>';
+}
+
+if (3 === 'Teste') {
+  echo 'É igual<br>';
+}
+
+$a = 4;
+$b = 4;
+$c = '4';
+
+if ($a === $b) {
+  echo 'A é igual a B <br>';
+}
+
+
+if ($a === $c) {
+  echo 'A é igual a B <br>';
+}
