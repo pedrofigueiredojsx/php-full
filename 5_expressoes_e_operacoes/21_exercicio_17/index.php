3dcd2e24b2addfedd6a309084db1149996bc559e
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-06 12:50:45 -0300

    feat: exericio com and

---
+++ 5_expressoes_e_operacoes/21_exercicio_17/index.php
@@ -0,0 +1,17 @@
+<?php
+
+if (15 > 5 && "João" === "João") {
+  echo "if 1 true<br>";
+}
+
+if ('teste' > 5 && 1) {
+  echo "if 2 true<br>";
+}
+
+if (2 == 3 && 5 >= 3) {
+  echo "if 3 true<br>";
+}
+
+if (10 > 5 && 1) {
+  echo "if 4 true<br>";
+}
