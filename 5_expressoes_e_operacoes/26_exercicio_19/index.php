7e410d3198a94d7f65207df17f07b11dcfc8c3e7
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-11 12:14:43 -0300

    feat: exercicio type cast

---
+++ 5_expressoes_e_operacoes/26_exercicio_19/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = (int) "testando";
+echo $a . "<br>";
+$b = (int) "aaa";
+echo $b . "<br>";
+
+
+$c = (int) 12.9;
+echo $c . "<br>";
+$d = (int) 5;
+echo $d . "<br>";
+
+$e = (int) TRUE;
+echo $e . "<br>";
+$f = (int) FALSE;
+echo $f . "<br>";
+
+$g = (int) [1, 2, 3];
+echo $g . "<br>";
+$h = (int) [1, 2, 3, 4, 5, 6];
+echo $h . "<br>";
