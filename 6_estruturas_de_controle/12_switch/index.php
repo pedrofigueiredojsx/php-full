c5bf3f5b93fbafc330d4abfa5cc4caa0abf36972
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-26 18:08:12 -0300

    feat: switch case

---
+++ 6_estruturas_de_controle/12_switch/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$x = 5;
+
+switch ($x) {
+  case 0:
+    echo "X é igual a 0 <br>";
+    break;
+  case 1:
+    echo "X é igual 1 <br>";
+    break;
+  default:
+    echo "X não é igual a nenhum dos valores mencionados <br>";
+}
+
+$y = "Pedro";
+
+switch ($y) {
+  case "Pedro":
+    echo "O nome é Pedro <br>";
+    break;
+  case "Breno":
+    echo "O nome é Breno <br>";
+    break;
+  default:
+    echo "X não é igual a nenhum dos valores mencionados <br>";
+}
