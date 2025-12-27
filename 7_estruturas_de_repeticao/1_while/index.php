1468c8417fb152534555f96fc10eb3c8a547de19
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-27 14:51:48 -0300

    feat: while

---
+++ 7_estruturas_de_repeticao/1_while/index.php
@@ -0,0 +1,64 @@
+<?php
+
+// A difinição do contador
+$x = 0;
+
+// Inicio / Definicao da estrutura
+while ($x < 10) {
+
+  // Corpo do loop
+  echo $x . "<br>";
+
+  // Incremento do contador
+  $x =  $x + 1;
+}
+
+
+echo "Continuando código <br>";
+
+// A difinição do contador
+$y = 0;
+
+// Inicio / Definicao da estrutura
+while ($y <= 10) {
+
+  // Corpo do loop
+  echo $y . "<br>";
+
+  // Incremento do contador
+  $y += 2;
+}
+
+echo "Continuando código <br>";
+
+// A difinição do contador
+$z = 10;
+
+// Inicio / Definicao da estrutura
+while ($z > 0) {
+
+  // Corpo do loop
+  echo $z . "<br>";
+
+  // Incremento do contador
+  $z--;
+}
+
+echo "Continuando código <br>";
+
+// A difinição do contador
+$a = 10;
+
+// Inicio / Definicao da estrutura
+while ($a > 0) {
+
+  // Corpo do loop
+  if ($a % 2 != 0) {
+    echo $a . "<br>";
+  }
+
+  // Incremento do contador
+  $a--;
+}
+
+echo "Continuando código <br>";
