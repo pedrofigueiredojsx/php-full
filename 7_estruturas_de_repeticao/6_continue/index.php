fc08369512bafaafcf4b227ff40a3055572c7748
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2026-01-01 17:57:54 -0300

    feat: continue

---
+++ 7_estruturas_de_repeticao/6_continue/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+
+while ($a > 0) {
+
+  if ($a == 5 || $a == 7) {
+    echo "Pulou a execução $a <br>";
+
+    $a--;
+
+    continue;
+  }
+
+  if ($a == 2) {
+
+    echo "Terminando o loop break 2";
+
+    break;
+  }
+
+  echo "Executando o loop $a <br>";
+
+  $a--;
+}
